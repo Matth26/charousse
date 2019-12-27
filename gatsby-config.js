55d382047e4ca0708f2a941089e7e6bd37c12687
Matthias <matthias.monnier@gmail.com>
2019-12-27 18:32:54 +0100

    Add Liens and Tarifs page + update css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,20 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `Charousse`
   },
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken: process.env.DATO_API_TOKEN,
-      },
-    },
-  ],
-}
+        apiToken: process.env.DATO_API_TOKEN
+      }
+    }
+  ]
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,102 +1,130 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
-import { StaticQuery, graphql } from "gatsby"
-import { HelmetDatoCms } from 'gatsby-source-datocms'
+import React from 'react';
+import { Link } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
+import { HelmetDatoCms } from 'gatsby-source-datocms';
 
-import '../styles/index.sass'
+import '../styles/index.sass';
 
-const TemplateWrapper = ({ children }) => (
-  <StaticQuery query={graphql`
-    query LayoutQuery
-    {
-      datoCmsSite {
-        globalSeo {
-          siteName
-        }
-        faviconMetaTags {
-          ...GatsbyDatoCmsFaviconMetaTags
-        }
-      }
-      datoCmsHome {
-        seoMetaTags {
-          ...GatsbyDatoCmsSeoMetaTags
-        }
-        introTextNode {
-          childMarkdownRemark {
-            html
-          }
-        }
-        copyright
-      }
-      allDatoCmsSocialProfile(sort: { fields: [position], order: ASC }) {
-        edges {
-          node {
-            profileType
-            url
-          }
-        }
-      }
-    }
-  `}
-  render={data => (
-    <div className="container">
-      <HelmetDatoCms
-        favicon={data.datoCmsSite.faviconMetaTags}
-        seo={data.datoCmsHome.seoMetaTags}
-      />
-      <div className="container__sidebar">
-        <div className="sidebar">
-          <h6 className="sidebar__title">
-            <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-          </h6>
-          <div
-            className="sidebar__intro"
-            dangerouslySetInnerHTML={{
-              __html: data.datoCmsHome.introTextNode.childMarkdownRemark.html,
-            }}
-          />
-          <ul className="sidebar__menu">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-          <p className="sidebar__social">
-            {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
-              <a
-                key={profile.profileType}
-                href={profile.url}
-                target="blank"
-                className={`social social--${profile.profileType.toLowerCase()}`}
-              > </a>
-            ))}
-          </p>
-          <div className="sidebar__copyright">{data.datoCmsHome.copyright}</div>
-        </div>
-      </div>
-      <div className="container__body">
-        <div className="container__mobile-header">
-          <div className="mobile-header">
-            <div className="mobile-header__menu">
-              <Link to="#" data-js="toggleSidebar" />
-            </div>
-            <div className="mobile-header__logo">
-              <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-            </div>
+class Layout extends React.Component {
+  state = { isOpen: false };
+
+  toggleSidebar = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className={this.state.isOpen ? 'container is-open' : 'container'}>
+        <HelmetDatoCms
+          favicon={this.props.data.datoCmsSite.faviconMetaTags}
+          seo={this.props.data.datoCmsHome.seoMetaTags}
+        />
+        <div className="container__sidebar">
+          <div className="sidebar">
+            <h6 className="sidebar__title">
+              <Link to="/">{this.props.data.datoCmsSite.globalSeo.siteName}</Link>
+            </h6>
+            <div
+              className="sidebar__intro"
+              dangerouslySetInnerHTML={{
+                __html: this.props.data.datoCmsHome.introTextNode.childMarkdownRemark.html
+              }}
+            />
+            <ul className="sidebar__menu">
+              <li>
+                <Link to="/">Accueil</Link>
+              </li>
+              <li>
+                <Link to="/presentation">Présentation</Link>
+              </li>
+              <li>
+                <Link to="/calendrier">Calendrier</Link>
+              </li>
+              <li>
+                <Link to="/tarifs">Tarifs</Link>
+              </li>
+              <li>
+                <Link to="/presentation">Vallée de Sye et Gervanne</Link>
+              </li>
+              <li>
+                <Link to="/presentation">Plan d'accès</Link>
+              </li>
+              <li>
+                <Link to="/liens">Liens</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+            <p className="sidebar__social">
+              {this.props.data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
+                <a
+                  key={profile.profileType}
+                  href={profile.url}
+                  target="blank"
+                  className={`social social--${profile.profileType.toLowerCase()}`}>
+                  {' '}
+                </a>
+              ))}
+            </p>
+            <div className="sidebar__copyright">{this.props.data.datoCmsHome.copyright}</div>
           </div>
         </div>
-        {children}
+        <div className="container__body">
+          <div className="container__mobile-header">
+            <div className="mobile-header">
+              <div className="mobile-header__menu" onClick={this.toggleSidebar}>
+                <div></div>
+              </div>
+              <div className="mobile-header__logo">
+                <Link to="/">{this.props.data.datoCmsSite.globalSeo.siteName}</Link>
+              </div>
+            </div>
+          </div>
+          <div className="charousse_header">CHAROUSSE</div>
+          {this.props.children}
+        </div>
       </div>
-    </div>
-    )}
-  />
-)
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.object,
+    );
+  }
 }
 
-export default TemplateWrapper
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query Layout2Query {
+        datoCmsSite {
+          globalSeo {
+            siteName
+          }
+          faviconMetaTags {
+            ...GatsbyDatoCmsFaviconMetaTags
+          }
+        }
+        datoCmsHome {
+          seoMetaTags {
+            ...GatsbyDatoCmsSeoMetaTags
+          }
+          introTextNode {
+            childMarkdownRemark {
+              html
+            }
+          }
+          copyright
+        }
+        allDatoCmsSocialProfile(sort: { fields: [position], order: ASC }) {
+          edges {
+            node {
+              profileType
+              url
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Layout data={data} {...props} />}
+  />
+);
