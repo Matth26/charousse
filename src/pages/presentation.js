26cf78d8c20b64e7510fccd17dae41c79297fd23
Matthias <matthias.monnier@gmail.com>
2025-06-02 10:45:47 +0200

    add slideshow to presentation page

--- src/pages/presentation.js
+++ src/pages/presentation.js
@@ -4,29 +4,39 @@ import Img from 'gatsby-image';
 import Layout from '../components/layout';
 
 import BackgroundImage from 'gatsby-background-image';
+import SlideShow from '../components/SlideShow';
 
 function displayContent(block, i) {
+  console.log(block);
   if (block.image !== undefined) {
     return (
       <div key={i} className="sheet__gallery">
         <Img key={i} fluid={block.image.fluid} />
       </div>
     );
-  } else {
+  } else if (block.paragrapheNode !== undefined) {
     return (
       <div
-        key={i} 
+        key={i}
         className="sheet__body"
         dangerouslySetInnerHTML={{
-          __html: block.paragrapheNode.childMarkdownRemark.html
+          __html: block.paragrapheNode.childMarkdownRemark.html,
         }}
       />
+    );
+  } else if (block.images !== undefined) {
+    // SlideShow
+    return (
+      <div className="sheet__gallery">
+        <SlideShow images={block.images} />
+      </div>
     );
   }
 }
 
 const PresentationPage = ({ data: { page, allDatoCmsBackground } }) => {
   const contentArray = page.edges[0].node.pageContent;
+  console.log(contentArray);
   return (
     <BackgroundImage
       Tag="section"
@@ -37,7 +47,9 @@ const PresentationPage = ({ data: { page, allDatoCmsBackground } }) => {
       <Layout>
         <article className="sheet">
           <h1 className="sheet__title">Présentation</h1>
-          <div className="sheet__inner">{contentArray.map((block, i) => displayContent(block, i))}</div>
+          <div className="sheet__inner">
+            {contentArray.map((block, i) => displayContent(block, i))}
+          </div>
         </article>
       </Layout>
     </BackgroundImage>
@@ -64,7 +76,21 @@ export const query = graphql`
             ... on DatoCmsImage {
               id
               image {
-                fluid(maxWidth: 530, imgixParams: { fm: "jpg", auto: "compress" }) {
+                fluid(
+                  maxWidth: 530
+                  imgixParams: { fm: "jpg", auto: "compress" }
+                ) {
+                  ...GatsbyDatoCmsSizes
+                }
+              }
+            }
+            ... on DatoCmsSlideshow {
+              id
+              images {
+                fluid(
+                  maxWidth: 530
+                  imgixParams: { fm: "jpg", auto: "compress" }
+                ) {
                   ...GatsbyDatoCmsSizes
                 }
               }
@@ -79,7 +105,10 @@ export const query = graphql`
           id
           title
           source {
-            fluid(maxWidth: 1000, imgixParams: { fm: "jpg", auto: "compress" }) {
+            fluid(
+              maxWidth: 1000
+              imgixParams: { fm: "jpg", auto: "compress" }
+            ) {
               ...GatsbyDatoCmsSizes
             }
           }
